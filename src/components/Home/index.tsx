49e5c42047d26dac1b739ce13da556e5585339e7
Danilo <danilocbertolini@gmail.com>
2023-07-02 17:25:59 -0300

    .

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -3,13 +3,16 @@ import './home.css'
 
 function Home() {
     return (
-        <div className='home'>
-            <h2>
-                Projetos Pessoais
-                {' '}
-                <Ghost size={35} />
-            </h2>
-        </div>
+        <div>
+            <img src="https://64.media.tumblr.com/1e59432e5fe2d7942df39fdd0223d294/9ec5a9ae5ba9f7db-ae/s1280x1920/8f45c6df3adc9f1a74b58a275b587982b5de8abc.gifv" alt="mario" />
+            <div className='home'>
+                <h2>
+                    Projetos Pessoais
+                    {' '}
+                    <Ghost size={35} />
+                </h2>
+            </div>
+        </ div>
     )
 }
 
