13d9b2e39573bbe53cca9274a610c4f844756756
Danilo <danilocbertolini@gmail.com>
2023-07-02 17:51:07 -0300

    .

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -3,7 +3,7 @@ import './home.css'
 
 function Home() {
     return (
-        <div>
+        <div className="homeFoto">
             <img src="https://64.media.tumblr.com/1e59432e5fe2d7942df39fdd0223d294/9ec5a9ae5ba9f7db-ae/s1280x1920/8f45c6df3adc9f1a74b58a275b587982b5de8abc.gifv" alt="mario" />
             <div className='home'>
                 <h2>
--- src/components/Projetos/index.tsx
+++ src/components/Projetos/index.tsx
@@ -12,12 +12,27 @@ function Projetos() {
           <GameController size={32} />
         </h2>
         <div>
-          <h3>
-            Básico
-            <br></br>
-            <Barricade size={32} />
-          </h3>
-          <h3>
+          <div className="projetosImg">
+            <h3>
+              Jogo da Velha
+            </h3>
+            <a href="https://danilobertolini.github.io/Jogo-da-Velha/">
+              <img src="./jogodavelha.png" alt="jogo da velha" />
+            </a>
+          </div>
+          <div className="projetosImg">
+            <h3>
+              Construção
+              <Barricade size={32} />
+            </h3>
+          </div>
+          <div className="projetosImg">
+            <h3>
+              Construção
+              <Barricade size={32} />
+            </h3>
+          </div>
+          {/* <h3>
             Intermediário
             <br></br>
             <Barricade size={32} />
@@ -26,7 +41,7 @@ function Projetos() {
             Avançado
             <br></br>
             <Barricade size={32} />
-          </h3>
+          </h3> */}
         </div>
       </section>
     )
